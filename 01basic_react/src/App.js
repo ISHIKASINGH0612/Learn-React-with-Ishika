26299adb20574770f511137a0733d63984ae1eee
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2026-01-31 22:23:48 +0530

    files created

---
+++ 01basic_react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <div >Learn React with Hitesh</div>
+  );
+}
+
+export default App;
---
+++ 01vite_react/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="h1">Learn React with Hitesh</div>
+  )
+}
+
+export default App
