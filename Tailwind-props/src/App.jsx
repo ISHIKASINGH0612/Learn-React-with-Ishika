b27c4ae003a927ac2db2819e1aa0f2a891328fb5
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2026-02-17 18:08:08 +0530

    updated

---
+++ Tailwind-props/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Cards from './components/cards'
+// import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className=' text-3xl text-center font-bold text-blue-600 bg-green-100'>I want to be Happiiee</h1>
+
+      <Cards />
+      <Cards />
+
+    
+       
+    </>
+  )
+}
+
+export default App
