ef20c6f5ce353cc76fcfaf85bec55793dd695c77
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2026-02-17 18:07:28 +0530

    Project 1 completed

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
+
+  const[color, setColor]=useState("black")
+
+  // setcolor=({nextColor})=>
+  // {
+  //   color=nextColor
+  // }
   
 
   return (
-    <>
-      <h1>Kar Lungi Yarrr.......Karna to hai hi</h1>
-    </>
+    <div className="w-full h-screen duration-100" style= {{backgroundColor : color}}>
+    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+    <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+      <button onClick={() => setColor("Red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Red"}} >Red</button>
+      <button onClick={() => setColor("Green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Green"}}>Green</button>
+      <button onClick={() => setColor("Purple")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Purple"}}>Purple</button>
+      <button onClick={() => setColor("Blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Blue"}}>Blue</button>
+      <button onClick={() => setColor("Pink")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Pink"}}>Pink</button>
+      <button onClick={() => setColor("Yellow")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style= {{backgroundColor: "Yellow"}}>Yellow</button>
+      //onClick hamesha function maangta hai
+    </div>
+    </div>
+    </div>
   )
 }
 
